f5dc3c2284e6b97e1552df6dd10c2975ddcb0c0a
dariusz grubba <dariusz.grubba.pp@gmail.com>
2022-05-14 17:17:01 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import Controller from "./Controller";
+import "./css/Game.css";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import CustomNight from "./CustomNight";
+
+const initialState = {
+  mode: "NORMAL",
+  Freddy: 10,
+  Bonnie: 10,
+  Chica: 10,
+  Foxy: 10,
+};
+
+const Start = () => {
+  const [Start, setStart] = useState(false);
+  const [stages, setStages] = useState(initialState);
+  const [hourLength, setHourLength] = useState(89000);
+
+  useEffect(() => {
+    console.log(window.innerHeight > window.innerWidth);
+    if (window.innerHeight > window.innerWidth) {
+      window.alert(
+        "Rotate your phone to landscape mode for a better experience."
+      );
+    }
+  }, []);
+
+  return (
+    <>
+      {!Start ? (
+        <div className="custom-night">
+          <CustomNight
+            setStart={setStart}
+            state={{ ranges: stages, setStages, setHourLength }}
+            hourLength={hourLength}
+            setHourLength={setHourLength}
+          />
+        </div>
+      ) : (
+        <Controller stages={stages} setStart={setStart} hourLength={hourLength} />
+      )}
+    </>
+  );
+};
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Start />
+  </Provider>,
+  document.getElementById("root")
+);
+
+serviceWorker.register();
